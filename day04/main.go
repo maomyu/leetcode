78ed97248f455c89dbd0483266100cc510f905ee
yuwei1 <yuweijiubayiqi@163.com>
2019-11-23 20:58:58 +0800

    最长公共前缀

---
+++ day04/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	// 假设数组中长度最小的字符串为公共长度
+	minstr := strs[0]
+
+	for _, v := range strs {
+		if len(minstr) >= len(v) {
+			minstr = v
+		}
+	}
+	// fmt.Println(minstr)
+	for _, v := range strs {
+		minstr = findminstr(v, minstr)
+		if len(minstr) == 0 {
+			return ""
+		}
+	}
+	return minstr
+}
+func findminstr(str string, min string) string {
+	str = str[:len(min)]
+	for i, v := range str {
+		if v != rune(min[i]) {
+			min = str[:i]
+			break
+		}
+	}
+	return min
+}
+func main() {
+	strs := []string{"dog", "racecar", "car"}
+	fmt.Println(longestCommonPrefix(strs))
+}
