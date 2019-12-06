4ee63c763a1d07dc3ec84b218f9cf56521209fb1
yuwei1 <yuweijiubayiqi@163.com>
2019-12-06 20:20:22 +0800

    uodate

--- day05/main.go
+++ day05/main.go
@@ -32,7 +32,7 @@ func (s StringScilce) Swap(i, j int) {
 
 // 滑动窗口
 func checkInclusion(s1 string, s2 string) bool {
-	if len(s1) == 0 || len(s2) == 0 {
+	if len(s1) == 0 || len(s2) == 0 || len(s1) > len(s2) {
 		return false
 	}
 	sr1 := []rune(s1)
@@ -40,7 +40,7 @@ func checkInclusion(s1 string, s2 string) bool {
 	// 循环遍历长字符串
 	for i, _ := range s2 {
 		// 结束条件
-		if i == len(s2)-len(s1) {
+		if i == len(s2)-len(s1)+1 {
 			break
 		}
 		// 开始滑动窗口,获得需要比较的字符串
@@ -49,12 +49,54 @@ func checkInclusion(s1 string, s2 string) bool {
 		sr2 := []rune(str)
 		// 排序数组
 		sort.Sort(StringScilce(sr2))
+		fmt.Println(string(sr1))
+		fmt.Println(string(sr2))
 		if string(sr1) == string(sr2) {
 			return true
 		}
 	}
 	return false
 }
+
+func checkInclusion_one(s1 string, s2 string) bool {
+	if len(s1) == 0 || len(s2) == 0 || len(s1) > len(s2) {
+		return false
+	}
+
+	// 循环遍历长字符串
+	for i, _ := range s2 {
+		// 结束条件
+		if i == len(s2)-len(s1)+1 {
+			break
+		}
+		// 开始滑动窗口,获得需要比较的字符串
+		str := s2[i : i+len(s1)]
+		return StringContain(str, s1)
+	}
+	return true
+}
+
+// 蛮力轮询
+func StringContain(a string, b string) bool {
+	ar := []rune(a)
+	br := []rune(b)
+	// 用短字符串去寻找长字符串进行比较，因此主循环用短字符串进行控制
+	for i := 0; i < len(br); i++ {
+		// 定义一个 j 用来每次循环都要保证从 0 开始,j 代表 ar的下表
+		j := 0
+		// 循环长字符串
+		for j = 0; j < len(ar) && br[i] != ar[j]; j++ {
+
+		}
+		fmt.Println(j)
+		// 如果在遍历的过程中遇到不符合情况的字符，则上一个循环会比较到 ar 最后，可用 j 作为判断
+		// 如果在循环的过程中找到相同字符，第二个循环会提前结束
+		if j >= len(ar) {
+			return false
+		}
+	}
+	return true
+}
 func main() {
-	fmt.Println(checkInclusion("", "eidbaooo"))
+	fmt.Println(checkInclusion_one("adc", "dda"))
 }
