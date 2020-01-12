cdabe287ff7bdf2a3c66fdf9abe1d2190a417e02
yuwei1 <yuweijiubayiqi@163.com>
2020-01-12 12:15:27 +0800

    从排序数组中删除重复项

---
+++ newday/day01/demo1/demo1.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// 从排序数组中删除重复项
+func removeDuplicates(nums []int) int {
+	// 【0,0,1,1,1,2,2,3,3,4】
+	// 【0,1,0,1,1,2,2,3,3,4】
+	// 首指针
+	length := 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[length] {
+			length++
+			nums[length] = nums[i]
+			fmt.Println(nums)
+		}
+	}
+
+	return length + 1
+}
+
+func main() {
+	data := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(data))
+}
