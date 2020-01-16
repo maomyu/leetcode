1b291c1a48e7be4f905c2f7f2e5dfac4702afe3c
yuwei1 <yuweijiubayiqi@163.com>
2020-01-16 21:01:39 +0800

    外观数

--- newday/day02/demo8/demo8.go
+++ newday/day02/demo8/demo8.go
@@ -1,5 +1,11 @@
 package main
-/*
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/**
 1.     1
 2.     11
 3.     21
@@ -7,9 +13,28 @@ package main
 5.     111221
 */
 func countAndSay(n int) string {
-	
+	fpre := "1"
+	for i := 2; i <= n; i++ {
+		fnext := getstr(fpre)
+		fpre = fnext
+	}
+	return fpre
 }
-
+func getstr(str string) string {
+	result := ""
+	count := 1
+	for j := 1; j < len(str); j++ {
+		if str[j] == str[j-1] {
+			count++
+		} else {
+			result = result + strconv.FormatInt(int64(count), 10) + string(str[j-1])
+			count = 1
+		}
+	}
+	result = result + strconv.FormatInt(int64(count), 10) + string(str[len(str)-1])
+	return result
+}
 func main() {
-
+	// fmt.Println(getstr("1111"))
+	fmt.Println(countAndSay(4))
 }
