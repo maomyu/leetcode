360f6fc1a5f5ac920dff00e930947dd3a95f5fa7
yuwei1 <yuweijiubayiqi@163.com>
2020-02-04 09:51:36 +0800

    init

---
+++ newday/day04/demo4/demo4.go
@@ -0,0 +1,21 @@
+package main
+
+// 二叉树的层次遍历
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var m = [][]int
+var level []int
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return m
+	}
+	return m
+}
+
+func main() {
+}
